0628d8ed926f54a1d4ef4901286686a969155403
Shivam Maurya <172200378+ShivamMaurya2002@users.noreply.github.com>
2025-07-28 23:41:54 +0530

    Update Sum of Numbers.js

--- JavaScript Interview Programs/Sum of Numbers.js
+++ JavaScript Interview Programs/Sum of Numbers.js
@@ -4,30 +4,62 @@
     const a = 10;
     const b = 20;
     const sum = a + b;
-    console.log("Addition of two number", sum);
+    console.log("Addition of two number:", sum);
     // output: 
     // Addition of two number: 30
 
-// Using Function() 
-function sum(a, b){
-    const c = a + b;
-}
-return c;
-sum(4,5);
+// 2. Using Function()
 
+    // Function to calculate the sum of two numbers using a temporary variable:
+    function Sum(a, b){
+        const c = a + b;
+        return c;
+    }
+    console.log("Sum:", Sum(4,5));
+    // Output: 
+    // Sum: 9
 
-// 2. One-Line Addition (Used for Console testing or demo purpose)
-    Console.log(10+5);
+    // OR
+
+    // Function to return the sum of two numbers directly
+    function Sum(a, b) {
+        return a + b;
+    }
+    console.log(Sum(3, 7));
     // Output:
-    // 15
+    // 10 
 
-// 3. Using "prompt()" It take Inputs from User, Convert to number and Perform Addition – Runs only in browser
-    let a = Number(prompt("Enter first number:"));
-    let b = Number(prompt("Enter second number:"));
-    console.log("Sum:", a + b);
+// 3.  Using Arrow Function
+    // One-liner arrow function to add two numbers
+    const add = (a, b) => a + b;
+    console.log(add(10, 15)); 
+    // Output: 
+    // 25
+
+    // OR 
+    
+    // Arrow function to add two numbers using a variable and return
+    const Add = (a, b) => {
+        const sum = a + b;
+        return sum;
+    }
+    console.log("Sum of Two Number:", Add(3,2));
+    // Output: 
+    // 5
+
+// 4. Using Rest Parameters ( Used For Multiple Inputs)
+    function addAll(...nums) {
+      let sum = 0;
+      for (let n of nums) {
+        sum += n;
+      }
+      return sum;
+    }
+    console.log(addAll(1, 2, 3, 4)); 
     // Output:
-    // Enter frist number: 2
-    // Enter second number: 8
-    // Sum: 10
+    // 10
 
-// OR
+//5. Using prompt() (User Input) – only browser
+let arr = [1, 2, 3, 4];
+let Sum = arr.reduce((a, b) => a + b, 0);
+console.log("Sum:", sum); // Output: 10
