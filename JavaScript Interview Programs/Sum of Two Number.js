dadd8a074784814775a46ecb9f5c10fc2f04408a
Shivam Maurya <172200378+ShivamMaurya2002@users.noreply.github.com>
2025-07-28 22:00:12 +0530

    Update Sum of Two Number.js

--- JavaScript Interview Programs/Sum of Two Number.js
+++ JavaScript Interview Programs/Sum of Two Number.js
@@ -1,4 +1,23 @@
-const a = 10;
-const b = 20;
-const c = a + b;
-console.log(c)
+// Common Ways to Add Numbers In JavaScript
+
+// 1. Normal Variable Addition (Direct Way)
+    const a = 10;
+    const b = 20;
+    const sum = a + b;
+    console.log("Addition of two number": sum);
+    // output: 
+    // Addition of two number: 30
+
+// 2. One-Line Addition (Used for Console testing or demo purpose)
+    Console.log(10+5);
+    // Output:
+    // 15
+
+// 3. Using "prompt()" It take Inputs from User, Convert to number and Perform Addition – Runs only in browser
+    let a = Number(prompt("Enter first number:"));
+    let b = Number(prompt("Enter second number:"));
+    console.log("Sum:", a + b);
+    // Output:
+    // Enter frist number: 2
+    // Enter second number: 8
+    // Sum: 10
