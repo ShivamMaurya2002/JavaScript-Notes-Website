bbda63825de072fbea43e008e079864a7bd6051f
Shivam Maurya <172200378+ShivamMaurya2002@users.noreply.github.com>
2025-07-29 21:24:33 +0530

    Update Factorial.js

--- JavaScript Interview Programs/Factorial.js
+++ JavaScript Interview Programs/Factorial.js
@@ -2,13 +2,12 @@
 // ******************************************************** Factorial **************************************************
 
 // 1. Factorial of a number:
-
 // Using For Loop:
-function factorialLoop(n) {
+function factorial(n) {
     let fact = 1;
     for (let i = 1; i <= n; i++) {
-        fact *= i;
+        fact *= i; // fact = fact * i;
     }
     return fact;
 }
-console.log("Factorial (Loop):", factorialLoop(5));
+console.log("Factorial (using For Loop):", factorial(5));
