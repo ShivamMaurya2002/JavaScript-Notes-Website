628c144fe9c7dbcad000f22d1d4a02fdd3136e0d
Shivam Maurya <172200378+ShivamMaurya2002@users.noreply.github.com>
2025-08-01 23:07:44 +0530

    Add files via upload

---
+++ JavaScript Interview Programs/Generate Fibonacci.js
@@ -0,0 +1,46 @@
+
+// ********************************************************************************* fibonacci ***********************************************************************
+
+// 1. Generate fibonacci of a Number:
+
+// Generate Fibonacci of a number using for loop():
+function fibonacci(n) {
+    let num1 = 0, num2 = 1, next;
+    console.log("Fibonacci Sequence:");
+    for (let i = 1; i <= n; i++) {
+        console.log(num1);
+        next = num1 + num2;
+        num1 = num2;
+        num2 = next;
+    }
+}
+fibonacci(7);
+// Output:
+// Fibonacci Sequence:
+// 0
+// 1
+// 1
+// 2
+// 3
+// 5
+// 8
+
+// OR
+
+// Generate Fibonacci of a Number Using While Loop:
+function fibonacci(n) {
+  let num1 = 0, num2 = 1, count = 0;
+  console.log("Fibonacci Series:");
+  while (count < n) {
+    console.log(num1);
+    let temp = num1 + num2;
+    num1 = num2;
+    num2 = temp;
+    count++;
+  }
+}
+fibonacci(7);
+// Output: 
+// 0 1 1 2 3 5 8
+
+
