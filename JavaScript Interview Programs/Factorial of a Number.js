cf692b230082e5172ede07369cfa7e417979ac89
Shivam Maurya <172200378+ShivamMaurya2002@users.noreply.github.com>
2025-08-01 21:34:59 +0530

    Update Factorial of a Number.js

--- JavaScript Interview Programs/Factorial of a Number.js
+++ JavaScript Interview Programs/Factorial of a Number.js
@@ -2,22 +2,17 @@
 // ****************************************************************************** Factorial *******************************************************************************
 
 // 1. Find Factorial of a Number:
-
-// Find factorial using For loop:
+// Find factorial using loop:
 function factorial(n) {
     let fact = 1;
     for (let i = 1; i <= n; i++) {
-        fact *= i; 
-        // fact = fact * i;
+        fact *= i; // fact = fact * i;
     }
     return fact;
 }
 console.log("Factorial using For Loop:", factorial(5));
-
 // Output: 
-// Factorial using For Loop: 120
-
-// OR
+// Factorial (using For Loop): 120
 
 // Find factorial Using Recursion:
 function factorial(n) {
@@ -25,6 +20,21 @@ function factorial(n) {
     return n * factorial(n - 1);
 }
 console.log("Factorial using Recursion:", factorial(5));
+// Output:
+// Factorial using Recursion: 120
 
+// OR 
+
+// Find factorial Using Recursion:
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+const number = 5;
+const result = factorial(number);
+console.log("Factorial of", number, "is", result);
 // Output:
 // Factorial using Recursion: 120
