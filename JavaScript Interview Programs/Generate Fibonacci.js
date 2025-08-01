1e8c77b1b7f2757dfc381af298d11a06ffcfeb22
Shivam Maurya <172200378+ShivamMaurya2002@users.noreply.github.com>
2025-08-01 23:21:44 +0530

    Update Generate Fibonacci.js

--- JavaScript Interview Programs/Generate Fibonacci.js
+++ JavaScript Interview Programs/Generate Fibonacci.js
@@ -1,12 +1,15 @@
 
 // ********************************************************************************* fibonacci ***********************************************************************
 
+
+// ********************************************************************************* fibonacci ***********************************************************************
+
 // 1. Generate fibonacci of a Number:
 
 // Generate Fibonacci of a number using for loop():
-function fibonacci(n) {
+function fibonacciF(n) {
     let num1 = 0, num2 = 1, next;
-    console.log("Fibonacci Sequence:");
+    console.log("Fibonacci using for loop:");
     for (let i = 1; i <= n; i++) {
         console.log(num1);
         next = num1 + num2;
@@ -14,8 +17,9 @@ function fibonacci(n) {
         num2 = next;
     }
 }
-fibonacci(7);
-// Output: Fibonacci Sequence:
+fibonacciF(7);
+// Output:
+// Fibonacci Sequence:
 // 0
 // 1
 // 1
@@ -27,9 +31,9 @@ fibonacci(7);
 // OR
 
 // Generate Fibonacci of a Number Using While Loop:
-function fibonacci(n) {
+function fibonacciW(n) {
   let num1 = 0, num2 = 1, count = 0;
-  console.log("Fibonacci Series:");
+  console.log("Fibonacci Using While Loop:");
   while (count < n) {
     console.log(num1);
     let temp = num1 + num2;
@@ -38,8 +42,9 @@ function fibonacci(n) {
     count++;
   }
 }
-fibonacci(7);
-// Output: Fibonacci Sequence:
+fibonacciW(7);
+// Output: 
+// Fibonacci Sequence:
 // 0
 // 1
 // 1
@@ -48,19 +53,19 @@ fibonacci(7);
 // 5
 // 8
 
-
-// 2. Generate Fibonacci of a Number Using Recursion:
-function fibonacci(n) {
+// 2.  Generate Fibonacci of a Number Using Recursion
+function fibonacciR(n) {
     if (n <= 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return fibonacciR(n - 1) + fibonacciR(n - 2);
 }
 // Print first N Fibonacci numbers
 let n = 7;
-console.log("Fibonacci using recursion:");
+console.log("Fibonacci using Recursion:");
 for (let i = 0; i < n; i++) {
-    console.log(fibonacci(i));
+    console.log(fibonacciR(i));
 }
-// Output: Fibonacci Sequence:
+// Output:
+// Fibonacci Sequence:
 // 0
 // 1
 // 1
